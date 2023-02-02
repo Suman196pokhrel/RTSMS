4aa8df31380963b96dd9fff92b002b92af554298
Suman Pokhrel <spokhrel196@gmail.com>
2023-02-02 19:02:42 +0545

    Initializing project FE

---
+++ frontend/rtsms/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  console.log("Hello react")
+  return (
+    <div className="App">
+      <div>Test Application 101</div>
+    </div>
+  );
+}
+
+export default App;
