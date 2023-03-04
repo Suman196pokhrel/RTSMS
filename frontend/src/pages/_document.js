cfa0e6b3bf6e6c69092ea0718160f42d118f12c8
Suman Pokhrel <spokhrel196@gmail.com>
2023-03-04 16:30:25 +0545

    MUI configured

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from '../../utils/createEmotionCache';
-import {theme} from "../../utils/theme";
+import { theme } from "../../utils/theme";
 
 export default class MyDocument extends Document {
 	render() {
@@ -17,9 +17,14 @@ export default class MyDocument extends Document {
 					<link
 						rel="stylesheet"
 						href=
-"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+						"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
-{/* Inject MUI styles first to match with the prepend: true configuration. */}
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+					/>
+
+					{/* Inject MUI styles first to match with the prepend: true configuration. */}
 					{this.props.emotionStyleTags}
 				</Head>
 				<body>
@@ -36,14 +41,14 @@ export default class MyDocument extends Document {
 
 
 MyDocument.getInitialProps = async (ctx) => {
-	
+
 	const originalRenderPage = ctx.renderPage;
 
 	// You can consider sharing the same emotion cache between
 	// all the SSR requests to speed up performance.
 	// However, be aware that it can have global side effects.
-	
-const cache = createEmotionCache();
+
+	const cache = createEmotionCache();
 	const { extractCriticalToChunks } = createEmotionServer(cache);
 
 	ctx.renderPage = () =>
@@ -58,8 +63,8 @@ const cache = createEmotionCache();
 
 	// This is important. It prevents emotion to render invalid HTML.
 	// See
-// https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-	
+	// https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+
 	const emotionStyles = extractCriticalToChunks(initialProps.html);
 	const emotionStyleTags = emotionStyles.styles.map((style) => (
 		<style
