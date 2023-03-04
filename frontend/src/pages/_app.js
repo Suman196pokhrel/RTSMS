c36cbc603eb1e54d6fb5dc34a4ce7166e9f059ad
Suman Pokhrel <spokhrel196@gmail.com>
2023-03-04 16:08:37 +0545

    configured next.js project for MUI usage

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/system';
+import createEmotionCache from '../../utils/createEmotionCache';
+import { theme } from '../../utils/theme';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+const clientSideEmotionCache = createEmotionCache();
+
+
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+  return (
+
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+
+    </CacheProvider>
+
+
+  )
 }
